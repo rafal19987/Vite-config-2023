ddaaa159651006bec44f1e9e8fb0229cabe0719f
rafal19987 <rafal987.1@o2.pl>
2023-04-12 14:38:13 +0200

    Inicial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+const title = 'React';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
